f6203e13de7bea352e78bdb6d2765e24e18d3814
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-16 12:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ let table = document.getElementById("sampleTable");
 
 function insert_Row() {
     let newRow = document.createElement("tr");
-    newRow.innerHTML = `
-        <td>New Cell1</td>
-        <td>New Cell2</td>
-    `;
-    console.log("Inserted new row: ", newRow);  
-    table.prepend(newRow);  
+
+    let newCell1 = document.createElement("td");
+    newCell1.textContent = "New Cell1";
+    
+    let newCell2 = document.createElement("td");
+    newCell2.textContent = "New Cell2";
+    
+    newRow.appendChild(newCell1);
+    newRow.appendChild(newCell2);
+    
+    console.log("New row inserted:", newRow);
+    
+    
+    table.prepend(newRow);
 }
